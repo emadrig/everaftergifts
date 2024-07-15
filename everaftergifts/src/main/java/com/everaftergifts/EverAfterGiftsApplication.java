be8c1cf05bbd27c924a2d53d5bce9a2420d5b84a
Edwin Madrigal <emadrig1@uci.edu>
2024-07-15 16:24:23 -0700

    Initial commit

---
+++ everaftergifts/src/main/java/com/everaftergifts/EverAfterGiftsApplication.java
@@ -0,0 +1,13 @@
+package com.everaftergifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverAfterGiftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverAfterGiftsApplication.class, args);
+	}
+
+}
---
+++ everaftergifts/src/test/java/com/everaftergifts/EverAfterGiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everaftergifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverAfterGiftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
